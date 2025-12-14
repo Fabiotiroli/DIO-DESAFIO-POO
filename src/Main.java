d3118c8987c8a3131dfc7538ecdcc326d5252be9
Fabio Tiroli <fabiolobotiroli@gmail.com>
2025-12-14 16:21:14 -0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import domain.Curso;
+import domain.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        Curso curso = new Curso();
+        curso.setTitulo("Curso 1");
+        curso.setDescricao("Teste");
+        curso.setCargaHoraria(64);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria 1");
+        mentoria.setDataLancamento(LocalDate.now());
+        mentoria.setDataLancamento(LocalDate.of(2025, 12, 25));
+        mentoria.setDescricao("teste");
+
+        System.out.println(mentoria);
+        System.out.println(mentoria.getDescricao());
+        System.out.println(mentoria.getDataLancamento());
+        System.out.println(mentoria.getTitulo());
+
+
+        System.out.println(curso);
+        System.out.println(curso.getTitulo());
+        System.out.println(curso.getDescricao());
+        System.out.println(curso.getCargaHoraria());
+
     }
 }
---
+++ src/domain/Mentoria.java
@@ -0,0 +1,48 @@
+package domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Mentoria {
+
+    private String titulo;
+    private String descricao;
+    private LocalDate dataLancamento;
+
+
+    public Mentoria() {}
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", dataLancamento=" + dataLancamento +
+                '}';
+    }
+}
